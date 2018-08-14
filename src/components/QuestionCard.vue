f06501e069f722bdf2476d7009abed497e9eeee8
Philipp Munzert <mail@phmu.de>
2018-08-14 12:34:19 +0200

    added slide-in transition for questions

--- src/components/QuestionCard.vue
+++ src/components/QuestionCard.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class=" flex justify-center p-4 container mx-auto">
-      <div class="max-w-sm border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal shadow">
+  <transition name="fade">
+    <div class="question-card flex justify-center p-4 container mx-auto" @click="markAnswered">
+      <div class="max-w-sm border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded p-4 flex flex-col justify-between leading-normal shadow">
         <div class="mb-8">
           <div class="text-black font-bold text-xl mb-2">Can coffee make you a better developer?</div>
           <p class="text-grey-darker text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
@@ -14,13 +15,56 @@
         </div>
       </div>
     </div>
+    </transition>
 </template>
 
 <script>
 export default {
-  name: "QuestionCard"
-};
+  name: "QuestionCard",
+  methods: {
+    markAnswered: function (event) {
+      console.log(this, event)
+    }
+  }
+}
+
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.question-card {
+  animation: slide-in 0.75s cubic-bezier(0.51, 0.42, 0.55, 1.1);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  animation: slide-in 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  animation: slide-out 0.5s;
+}
+
+@keyframes slide-in {
+  0% {
+    opacity: 0;
+    transform: translateY(100vh);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+@keyframes slide-out {
+  0% {
+    transform: translateY(50px);
+  }
+
+  100% {
+    opacity: 0;
+    transform: translateY(-50px);
+  }
+}
 </style>
